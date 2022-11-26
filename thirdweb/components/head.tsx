e2e8a93f6f84597d78695acd6c8fb99f1a909d2e
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-26 23:11:31 +0900

    update home page

---
+++ thirdweb/components/head.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Head from 'next/head';
+
+export default function HeadComponent(): JSX.Element {
+  return (
+    <Head>
+      <title>Create a DAO tool from scratch</title>
+    </Head>
+  ); 
+}
