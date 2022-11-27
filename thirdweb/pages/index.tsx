200d0499a87038754a399cf955c248e68db82239
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-27 14:13:37 +0900

    Update

--- thirdweb/pages/index.tsx
+++ thirdweb/pages/index.tsx
@@ -1,4 +1,7 @@
 import { useState, useEffect, useMemo } from "react";
+import { Proposal } from "@thirdweb-dev/sdk";
+import { AddressZero } from "@ethersproject/constants";
+
 import type { NextPage } from "next";
 // 接続中のネットワークを取得するため useNetwork を新たにインポートします。
 import { ConnectWallet, ChainId, useNetwork, useAddress, useContract } from "@thirdweb-dev/react";
@@ -16,6 +19,9 @@ const Home: NextPage = () => {
   // Tokenコントラクトを初期化
   const token = useContract("0x682DBa03EB560200984e67804d2884CeDb8F506c", "token").contract;
 
+  // 投票コントラクトの初期化
+  const vote = useContract("0x1F3fBA0aCFBFA8198B275Dc8eEFE63f4bEc957D9", "vote").contract;
+
   // ユーザーがメンバーシップ NFT を持っているかどうかを知るためのステートを定義
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
 
@@ -24,16 +30,67 @@ const Home: NextPage = () => {
 
   // メンバーごとの保有しているトークンの数をステートとして宣言
   const [memberTokenAmounts, setMemberTokenAmounts] = useState<any>([]);
-  
+
   // DAO メンバーのアドレスをステートで宣言
   const [memberAddresses, setMemberAddresses] = useState<string[] | undefined>([]);
+
+  const [proposals, setProposals] = useState<Proposal[]>([]);
+  const [isVoting, setIsVoting] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
 
   // アドレスの長さを省略してくれる便利な関数
   const shortenAddress = (str: string) => {
     return str.substring(0, 6) + "..." + str.substring(str.length - 4);
   };
 
-  // メンバーシップを保持しているメンバーの全アドレスを取得します
+  // コントラクトから既存の提案を全て取得
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // vote!.getAll() を使用して提案を取得
+    const getAllProposals = async () => {
+      try {
+        const proposals = await vote!.getAll();
+        setProposals(proposals);
+        console.log("🌈 Proposals:", proposals);
+      } catch (error) {
+        console.log("failed to get proposals", error);
+      }
+    };
+    getAllProposals();
+  }, [hasClaimedNFT, vote]);
+
+  // ユーザーがすでに投票したかどうか確認
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // 提案を取得し終えない限り、ユーザーが投票したかどうかを確認することができない
+    if (!proposals.length) {
+      return;
+    }
+
+    const checkIfUserHasVoted = async () => {
+      try {
+        const hasVoted = await vote!.hasVoted(proposals[0].proposalId.toString(), address);
+        setHasVoted(hasVoted);
+        if (hasVoted) {
+          console.log("🥵 User has already voted");
+        } else {
+          console.log("🙂 User has not voted yet");
+        }
+      } catch (error) {
+        console.error("Failed to check if wallet has voted", error);
+      }
+    };
+    checkIfUserHasVoted();
+
+  }, [hasClaimedNFT, proposals, address, vote]);
+
+  // メンバーシップを保持しているメンバーの全アドレスを取得
   useEffect(() => {
     if (!hasClaimedNFT) {
       return;
@@ -78,7 +135,7 @@ const Home: NextPage = () => {
       // memberTokenAmounts 配列でアドレスが見つかっているかどうかを確認します
       // その場合、ユーザーが持っているトークンの量を返します
       // それ以外の場合は 0 を返します
-      const member = memberTokenAmounts?.find(({ holder }: {holder: string}) => holder === address);
+      const member = memberTokenAmounts?.find(({ holder }: { holder: string }) => holder === address);
 
       return {
         address,
@@ -127,7 +184,7 @@ const Home: NextPage = () => {
       setIsClaiming(false);
     }
   };
-  
+
   // ウォレットと接続していなかったら接続を促す
   if (!address) {
     return (
@@ -158,40 +215,157 @@ const Home: NextPage = () => {
       </div>
     );
   }
-    // DAO ダッシュボード画面を表示
-    else if (hasClaimedNFT){
-      return (
-        <div className={styles.container}>
+  // DAO ダッシュボード画面を表示
+  else if (hasClaimedNFT) {
+    return (
+      <div className={styles.container}>
         <main className={styles.main}>
-          <h1 className={styles.title}>🍪DAO Member Page</h1>
-          <p>Congratulations on being a member</p>
+        <h1 className={styles.title}>🍪DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
           <div>
-          <div>
-            <h2>Member List</h2>
-            <table className="card">
-              <thead>
-                <tr>
-                  <th>Address</th>
-                  <th>Token Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {memberList!.map((member) => {
-                  return (
-                    <tr key={member.address}>
-                      <td>{shortenAddress(member.address)}</td>
-                      <td>{member.tokenAmount}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            <div>
+              <h2>■ Member List</h2>
+              <table className="card">
+                <thead>
+                  <tr>
+                    <th>Address</th>
+                    <th>Token Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {memberList!.map((member) => {
+                    return (
+                      <tr key={member.address}>
+                        <td>{shortenAddress(member.address)}</td>
+                        <td>{member.tokenAmount}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <div>
+              <h2>■ Active Proposals</h2>
+              <form
+                onSubmit={async (e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+
+                  // ダブルクリックを防ぐためにボタンを無効化します
+                  setIsVoting(true);
+
+                  // フォームから値を取得します
+                  const votes = proposals.map((proposal) => {
+                    const voteResult = {
+                      proposalId: proposal.proposalId,
+                      vote: 2,
+                    };
+                    proposal.votes.forEach((vote) => {
+                      const elem = document.getElementById(
+                        proposal.proposalId + "-" + vote.type
+                      ) as HTMLInputElement;
+
+                      if (elem!.checked) {
+                        voteResult.vote = vote.type;
+                        return;
+                      }
+                    });
+                    return voteResult;
+                  });
+
+                  // ユーザーが自分のトークンを投票に委ねることを確認する必要があります
+                  try {
+                    // 投票する前にウォレットがトークンを委譲する必要があるかどうかを確認します
+                    const delegation = await token!.getDelegationOf(address);
+                    // トークンを委譲していない場合は、投票前に委譲します
+                    if (delegation === AddressZero) {
+                      await token!.delegateTo(address);
+                    }
+                    // 提案に対する投票を行います
+                    try {
+                      await Promise.all(
+                        votes.map(async ({ proposalId, vote: _vote }) => {
+                          // 提案に投票可能かどうかを確認します
+                          const proposal = await vote!.get(proposalId);
+                          // 提案が投票を受け付けているかどうかを確認します
+                          if (proposal.state === 1) {
+                            return vote!.vote(proposalId.toString(), _vote);
+                          }
+                          return;
+                        })
+                      );
+                      try {
+                        // 提案が実行可能であれば実行する
+                        await Promise.all(
+                          votes.map(async ({ proposalId }) => {
+                            const proposal = await vote!.get(proposalId);
+
+                            // state が 4 の場合は実行可能と判断する
+                            if (proposal.state === 4) {
+                              return vote!.execute(proposalId.toString());
+                            }
+                          })
+                        );
+                        // 投票成功と判定する
+                        setHasVoted(true);
+                        console.log("successfully voted");
+                      } catch (err) {
+                        console.error("failed to execute votes", err);
+                      }
+                    } catch (err) {
+                      console.error("failed to vote", err);
+                    }
+                  } catch (err) {
+                    console.error("failed to delegate tokens");
+                  } finally {
+                    setIsVoting(false);
+                  }
+                }}
+              >
+                {proposals.map((proposal) => (
+                  <div key={proposal.proposalId.toString()} className="card">
+                    <h5>{proposal.description}</h5>
+                    <div>
+                      {proposal.votes.map(({ type, label }) => (
+                        <div key={type}>
+                          <input
+                            type="radio"
+                            id={proposal.proposalId + "-" + type}
+                            name={proposal.proposalId.toString()}
+                            value={type}
+                            // デフォルトで棄権票をチェックする
+                            defaultChecked={type === 2}
+                          />
+                          <label htmlFor={proposal.proposalId + "-" + type}>
+                            {label}
+                          </label>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+                <p></p>
+                <button disabled={isVoting || hasVoted} type="submit">
+                  {isVoting
+                    ? "Voting..."
+                    : hasVoted
+                      ? "You Already Voted"
+                      : "Submit Votes"}
+                </button>
+                <p></p>
+                {!hasVoted && (
+                  <small>
+                    This will trigger multiple transactions that you will need to
+                    sign.
+                  </small>
+                )}
+              </form>
+            </div>
           </div>
-        </div>
         </main>
       </div>
-      );
-    }
+    );
+  }
   // ウォレットと接続されていたら Mint ボタンを表示
   else {
     return (
